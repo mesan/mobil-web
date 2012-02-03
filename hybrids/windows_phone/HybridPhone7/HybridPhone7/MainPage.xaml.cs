ea1844ade1f019a5a98806aef37b3ec03fecde28
Thomas Aaeng <thomasa@mesan.no>
2012-02-03 13:57:12 +0100

    external links

--- hybrids/windows_phone/HybridPhone7/HybridPhone7/MainPage.xaml.cs
+++ hybrids/windows_phone/HybridPhone7/HybridPhone7/MainPage.xaml.cs
@@ -11,49 +11,64 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 using System.Windows.Navigation;
+using Microsoft.Phone.Tasks;
 
 namespace HybridPhone7
 {
     public partial class MainPage : PhoneApplicationPage
     {
         private List<string> _backStack = new List<string>();
+        private const String SERVER_ADDRESS = "http://localhost:8080";
 
         // Constructor
         public MainPage()
         {
             InitializeComponent();
-            webBrowser1.Navigated += new EventHandler<System.Windows.Navigation.NavigationEventArgs>(webBrowser1_Navigated);
             webBrowser1.IsScriptEnabled = true;
 
 
             webBrowser1.Loaded += (object sender, RoutedEventArgs e) =>
             {
-                //
-                webBrowser1.Navigate(new Uri("http://localhost:8080", UriKind.Absolute));
+                webBrowser1.Navigate(new Uri(SERVER_ADDRESS, UriKind.Absolute));
             };
 
         }
 
+
+        private void webBrowser1_Navigating(object sender, NavigatingEventArgs e)
+        {
+            if (!e.Uri.AbsoluteUri.Contains(SERVER_ADDRESS))
+            {
+                e.Cancel = true;
+                WebBrowserTask task = new WebBrowserTask();
+                task.Uri = e.Uri;
+                task.Show();
+            }
+        }
+
         private void webBrowser1_Navigated(object sender, NavigationEventArgs e)
         {
-            string url = webBrowser1.Source.OriginalString;
-
-            // ensure all slashes are the same
-            // app\www/index.html
-            // see: https://issues.apache.org/jira/browse/CB-184
-            url = url.Replace("\\", "/");
-
-            if (_backStack.Count < 2)
+            if (e.Uri.AbsoluteUri.Contains(SERVER_ADDRESS))
             {
-                _backStack.Add(url);
-            }
-            else
-            {
-                // check whether the URL represents a backwards navigation
-                string previousPage = _backStack[_backStack.Count - 2];
-                if (previousPage == url)
+                string url = webBrowser1.Source.OriginalString;
+
+                // ensure all slashes are the same
+                // app\www/index.html
+                // see: https://issues.apache.org/jira/browse/CB-184
+                url = url.Replace("\\", "/");
+
+                if (_backStack.Count < 2)
                 {
-                    _backStack.RemoveAt(_backStack.Count - 1);
+                    _backStack.Add(url);
+                }
+                else
+                {
+                    // check whether the URL represents a backwards navigation
+                    string previousPage = _backStack[_backStack.Count - 2];
+                    if (previousPage == url)
+                    {
+                        _backStack.RemoveAt(_backStack.Count - 1);
+                    }
                 }
             }
         }
