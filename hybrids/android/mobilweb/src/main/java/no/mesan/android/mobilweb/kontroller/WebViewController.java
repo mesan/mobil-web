94e8380d298cc58dd169639e97b95b91f24ffa54
Christian Ihle <christiani@mesan.no>
2012-02-03 14:56:52 +0100

    La til slik at eksterne urler laster i nettleseren

--- hybrids/android/mobilweb/src/main/java/no/mesan/android/mobilweb/kontroller/WebViewController.java
+++ hybrids/android/mobilweb/src/main/java/no/mesan/android/mobilweb/kontroller/WebViewController.java
@@ -3,6 +3,8 @@ package no.mesan.android.mobilweb.kontroller;
 import no.mesan.android.mobilweb.R;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -28,10 +30,13 @@ public class WebViewController extends Activity {
         myWebView = (WebView) findViewById(R.id.webview);
 
         // Sørger for at klikk på linker åpner i appen, og ikke i nettleseren
-        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.setWebViewClient(new MyWebViewClient());
 
         // Laster fra lokal jetty
-        myWebView.loadUrl("http://10.0.2.2:8080");
+//        myWebView.loadUrl("http://10.0.2.2:8080");
+
+        // Laster samme app, ute på nettet
+        myWebView.loadUrl("http://mobil-web-server.appspot.com/");
     }
 
     /**
@@ -49,5 +54,22 @@ public class WebViewController extends Activity {
         // If it wasn't the BACK key or there's no web page history, bubble up to the default
         // system behavior (probably exit the activity)
         return super.onKeyDown(keyCode, event);
+    }
+
+    /**
+     * Klasse som sørger for at "lokale" url'er laster i app'en, og eksterne laster i nettleseren.
+     */
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(final WebView view, final String url) {
+            if (Uri.parse(url).getHost().equals("mobil-web-server.appspot.com")) {
+                // This is my web site, so do not override; let my WebView load the page
+                return false;
+            }
+            // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
+            final Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+            return true;
+        }
     }
 }
